af68d18b145246ba61991f2e167ae53712449415
SURBHI_SOMANATHE <surbhisomnathe7777@gmail.com>
2022-09-14 14:35:20 +0530

    upload

---
+++ server.js
@@ -0,0 +1,13 @@
+// import { application } from "express"
+
+const express = require('express')
+const app = express()
+
+app.get("/api", (req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree", "userFour"] })
+})
+
+
+app.listen(5000, () => {
+    console.log("Server started on port 5000")
+})
